604bf0de5b88f3b48775220be5996d7dc0218304
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2023-02-24 18:51:39 -0300

    alterado alguns arquivos

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,14 +1,15 @@
-export abstract class View<T>{
-  protected elemento: HTMLElement;
+export abstract class View<T> {
 
-  constructor(seletor: string) {
-    this.elemento = document.querySelector(seletor);
-  }
+    protected elemento: HTMLElement;
 
-  public update(model: T): void {
-    const template = this.template(model);
-    this.elemento.innerHTML = template;
-  }
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
 
-  protected abstract template(model: T): string;
+    public update(model: T): void {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+
+    protected abstract template(model: T): string;
 }
--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -1,32 +1,36 @@
-import { View } from "./view.js";
+import { View } from './view.js';
 export class NegociacoesView extends View {
     template(model) {
         return `
-    <table class="table table-hover table-bordered">
-        <thead>
-            <tr>
-                <th>DATA</th>
-                <th>QUANTIDADE</th>
-                <th>VALOR</th>
-                <th>VOLUME</th>
-            </tr>
-        </thead>
-        <tbody>
-        ${model
-            .lista()
-            .map((negociacao) => {
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${model.lista().map(negociacao => {
             return `
-            <tr>
-                <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
-                <td>${negociacao.quantidade}</td>
-                <td>${negociacao.valor}</td>
-                <td>${negociacao.volume}</td>
-            </tr>
-            `;
-        })
-            .join("")}
-        </tbody>
-    </table>
-    `;
+                        <tr>
+                            <td>${this.formatar(negociacao.data)}
+                            </td>
+                            <td>
+                                ${negociacao.quantidade}
+                            </td>
+                            <td>
+                                ${negociacao.valor}
+                            </td>
+                        </tr>
+                    `;
+        }).join('')}
+            </tbody>
+        </table>
+        `;
+    }
+    formatar(data) {
+        return new Intl.DateTimeFormat()
+            .format(data);
     }
 }
