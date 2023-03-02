2c2c19c5ba21ed77e4aca30bbeed6edfdae4fa89
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2023-03-02 19:21:00 -0300

    baixada aula do curso completa

--- app/dist/js/decorators/escapar.js
+++ app/dist/js/decorators/escapar.js
@@ -2,8 +2,9 @@ export function escapar(target, propertyKey, descriptor) {
     const metodoOriginal = descriptor.value;
     descriptor.value = function (...args) {
         let retorno = metodoOriginal.apply(this, args);
-        if (typeof retorno === "string") {
-            retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, "");
+        if (typeof retorno === 'string') {
+            retorno = retorno
+                .replace(/<script>[\s\S]*?<\/script>/, '');
         }
         return retorno;
     };
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,12 +1,25 @@
 export function domInjector(seletor: string) {
-  return function (target: any, propertyKey: string) {
-    const getter = function () {
-      const elemento = document.querySelector(seletor);
-      return elemento;
-    };
+    
+    return function(target: any, propertyKey: string) {
+        console.log(`Modificando protype ${target.constructor.name}
+             e adicionando getter para a propriedade ${propertyKey}`);
 
-    Object.defineProperty(target, propertyKey, {
-      get: getter,
-    });
-  };
+        let elemento: HTMLElement;
+
+        const getter = function() {
+            if (!elemento) {
+                elemento = <HTMLElement>document.querySelector(seletor);
+                console.log(`buscando elemento do DOM com o seletor 
+                 ${seletor} para injetar em ${propertyKey}`);
+            }
+            
+            return elemento;
+        }
+
+        Object.defineProperty(
+            target, 
+            propertyKey, 
+            { get: getter }
+        );
+    }
 }
